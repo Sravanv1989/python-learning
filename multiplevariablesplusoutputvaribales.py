f1b4cde60ea5e2cb88535e071d3d923597917fdb
Sravanv1989 <sravanv@maitravaruna.com>
2022-10-01 12:46:13 +0530

    first commit

---
+++ multiplevariablesplusoutputvaribales.py
@@ -0,0 +1,28 @@
+x, y, z = "Orange", "Banana", "Cherry"
+
+print(x)
+print(y)
+print(z)
+
+x = y = z = "Orange"
+
+print(x)
+print(y)
+print(z)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+
+print(x)
+print(y)
+print(z)
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
