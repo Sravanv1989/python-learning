a8899b0f98fc9ddd3fd86d45d97637374d762f6d
Sravanv1989 <sravanv@maitravaruna.com>
2022-10-01 12:50:01 +0530

    first commit

---
+++ slicingplusmodifyplusconcatenatestrings.py
@@ -0,0 +1,29 @@
+b = "Hello, World!"
+print(b[2:5])
+b = "Hello, World!"
+print(b[:5])
+b = "Hello, World!"
+print(b[2:])
+b = "Hello, World!"
+print(b[-5:-2])
+
+a = "Hello, World!"
+print(a.upper())
+a = "Hello, World!"
+print(a.lower())
+
+a = " Hello, World! "
+print(a.strip())
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+
+
